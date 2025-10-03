36e6e642001464d33ecda9f3277eda0fef6b54f0
newtonEMC2 <corbacho.golmar.alexis@gmail.com>
2025-10-03 02:22:22 +0200

    refactor: move UseWizardProps and UseWizardReturn interfaces to useWizard.controller.tsx for better organization

--- src/lib/common/types.ts
+++ src/lib/common/types.ts
@@ -8,20 +8,3 @@ export interface WizardStepData {
   isOpen: boolean
   isCompleted: boolean
 }
-
-export interface UseWizardProps {
-  steps: (Omit<WizardStepData, 'isOpen' | 'isCompleted'> & Partial<Pick<WizardStepData, 'isOpen' | 'isCompleted'>>)[]
-  initialStep?: string
-}
-
-export interface UseWizardReturn {
-  steps: WizardStepData[]
-  currentStepId: string
-  goToStep: (stepId: string) => void
-  goToNextStep: () => void
-  goToPreviousStep: () => void
-  completeStep: (stepId: string) => void
-  isFirstStep: boolean
-  isLastStep: boolean
-  canGoToNextStep: boolean
-}
--- src/lib/common/useWizard.controller.tsx
+++ src/lib/common/useWizard.controller.tsx
@@ -8,11 +8,25 @@
 
 import { useCallback, useMemo, useState } from 'react'
 
-import {
-  UseWizardProps,
-  UseWizardReturn,
-  WizardStepData,
-} from '../common/types'
+export interface UseWizardProps {
+  steps: (Omit<WizardStepData, 'isOpen' | 'isCompleted'> &
+    Partial<Pick<WizardStepData, 'isOpen' | 'isCompleted'>>)[]
+  initialStep?: string
+}
+
+export interface UseWizardReturn {
+  steps: WizardStepData[]
+  currentStepId: string
+  goToStep: (stepId: string) => void
+  goToNextStep: () => void
+  goToPreviousStep: () => void
+  completeStep: (stepId: string) => void
+  isFirstStep: boolean
+  isLastStep: boolean
+  canGoToNextStep: boolean
+}
+
+import { WizardStepData } from '../common/types'
 
 export const useWizardController = ({
   steps: initialSteps,
